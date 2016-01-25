d7cde2d4417e6f8349eeb444af622368b8c55114
Ivan <ratvien@gmail.com>
2016-01-25 19:31:03 +0600

    Redirect page error show main page layout.

--- redirect.php
+++ redirect.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: LIS
- * Date: 26.11.2015
- * Time: 23:11
- */
-
 require( __DIR__ . '/data/functions.php');
 $shortCode = $_GET['l'];
 try{
@@ -18,8 +11,40 @@ catch (\PDOException $e) {
 try{
     $longUrl = getUrl($shortCode,$pdo);
     header('Location: ' .$longUrl);
-    }
+}
 catch(\Exception $e){
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>URL Shortener</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+    <meta charset="UTF-8">
+    <style>
+        .bt {
+            border-top: 1px solid;
+        }
+        .ar {
+            text-align: right;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="jumbotron">
+        <h2><a href="index.php">URL Shortener</a></h2>
+
+        <p><?php
     echo $e->getMessage();
     exit;
 }
+?></p>
+    </div>
+</div>
+</body>
+</html>
+
+
+
